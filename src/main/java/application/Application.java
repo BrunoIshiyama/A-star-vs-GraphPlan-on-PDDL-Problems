ff9479da64d8acc90fb211958c7ae604593797ac
AnaCarbone <anabcarbone@gmail.com>
2019-09-16 22:43:06 -0300

    Domain and problem parser

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,9 +1,18 @@
 package application;
 
+import fr.uga.pddl4j.encoding.CodedProblem;
+import parser.Parser;
+import utils.Utils;
+
 //it's the main class to be run
 public class Application {
 
-	public static void main(String[] args) {
-		System.out.println("My Java Application");
-	}
+    public static void main(String[] args) {
+        System.out.println("My Java Application");
+        // TEST
+        Parser PDDLParser = new Parser();
+        CodedProblem codedProblem = PDDLParser
+                .parseDomainAndProblem(Utils.PATH_PDDL_FILES_TYREWORLD_DOMAIN, Utils.PATH_PDDL_FILES_TYREWORLD_PROBLEM);
+        System.out.println(codedProblem.getConstants().toString());
+    }
 }
--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -1,10 +1,45 @@
 package parser;
 
+import fr.uga.pddl4j.encoding.CodedProblem;
+import fr.uga.pddl4j.parser.ErrorManager;
+import fr.uga.pddl4j.planners.ProblemFactory;
+import utils.Utils;
+
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 //the class of a PDDLParser for example
 public class Parser {
-	public String parsePDDL(File PDDL) {
-		return null;
-	}
+
+    public String parsePDDL(File PDDL) {
+        return null;
+    }
+
+    /**
+     * Parse from domain and problem PDDL files to the java Object CodedProblem.
+     * Both files must be inside the folder pddl_files in resources.
+     *
+     * @return Java object with Domain and Problem attributes
+     */
+    public CodedProblem parseDomainAndProblem(String pathDomain, String pathProblem) {
+        try {
+            File domain = new File(this.getClass().getResource(pathDomain).toURI());
+            File problem = new File(this.getClass().getResource(pathProblem).toURI());
+
+            ProblemFactory problemFactory = ProblemFactory.getInstance();
+            ErrorManager errorManager = problemFactory.parse(domain, problem);
+            if (!errorManager.isEmpty()) {
+                errorManager.printAll();
+            }
+            return problemFactory.encode();
+        } catch (IOException e) {
+            System.out.println("Unexpected error when parsing the PDDL domain and problem description.");
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            System.out.println("Unexpected error when finding the domain or problem file.");
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
