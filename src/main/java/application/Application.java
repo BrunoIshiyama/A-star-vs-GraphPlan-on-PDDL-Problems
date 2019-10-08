e17b7a73c343bf653742dbb0ed8832030471bf69
AnaCarbone <anabcarbone@gmail.com>
2019-10-07 21:40:23 -0300

    ajustes finais

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -2,8 +2,6 @@ package application;
 
 import domain.AStar;
 import fr.uga.pddl4j.encoding.CodedProblem;
-import fr.uga.pddl4j.planners.statespace.hsp.HSP;
-import fr.uga.pddl4j.util.Plan;
 import parser.Parser;
 import utils.PDDLManager;
 import utils.Utils;
@@ -12,15 +10,9 @@ public class Application {
 
     public static void main(String[] args) {
         Parser parser = new Parser();
-        CodedProblem codedProblem = parser.parseDomainAndProblem(Utils.PATH_PDDL_FILES_DOMAIN, Utils.PATH_PDDL_FILES_PROBLEM);
+        CodedProblem codedProblem = parser.parseDomainAndProblem(Utils.PATH_PDDL_FILES_TYREWORLD_DOMAIN, Utils.PATH_PDDL_FILES_TYREWORLD_PROBLEM);
         PDDLManager.init(codedProblem);
         try {
-//            System.out.println(PDDLManager.getActionParameters("loosen"));
-//            System.out.println(PDDLManager.getActionPreconditions("loosen").cardinality());
-//
-//            HSP hsp = new HSP();
-//            Plan plan = hsp.search(codedProblem);
-//            System.out.println(codedProblem.toString(plan));
 
             AStar aStar = new AStar(codedProblem);
             System.out.println(aStar.search());
